318c4b6fcbf2a2c13fa00e693860fcb106fcdfdc
elxor <69245613+elxor@users.noreply.github.com>
2021-06-12 21:44:29 +0700

    initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# Webpack Starter Template
+Webpack starter template with live reloading dev server, support babel and sass/scss preprocessor
+
+## Usage
+
+### Install Dependencies
+
+```bash
+npm install
+```
+
+### Development Server
+
+```bash
+npm start
+```
+
+### Production Bundle
+
+```bash
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[contenthash].${ext}`;
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: ['@babel/polyfill','./index.js'],
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: isDev ? 'source-map' : false,
+    target: isDev ? 'web' : 'browserslist',
+    devServer: {
+        port: 3000,
+        hot: isDev
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            template: 'index.html',
+            minify: {
+                removeComments: isProd,
+                collapseWhitespace: isProd
+            }
+        }),
+        new CopyPlugin({
+            patterns: [
+                {from: path.resolve(__dirname, 'src/favicon.png'),
+                to: path.resolve(__dirname, 'dist')}
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                      presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    optimization : {
+        minimizer: [
+            '...',
+            new CssMinimizerPlugin()
+        ]
+    }
+}
